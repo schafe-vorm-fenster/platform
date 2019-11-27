a7a011f09dd42663699d9db5ff332ddab864c3bd
Jan-Henrik Hempel <jan@schafe-vorm-fenster.de>
2019-11-27 22:36:59 +0100

    Update of sanity studio. Branding of the studio.

---
+++ studio/logo/myLogo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './myLogo.css';
+
+const myLogo = () => (
+  <img className={styles.myLogo} src="/static/Schafe-vorm-Fenster-UG_Logo_green Avatar-01.png" alt="Schafe vorm Fenster"/>
+);
+
+export default myLogo;
