3913e29babc17cc5b5fb7a2f3f9784db498a3a74
schafevormfenster <jan@schafe-vorm-fenster.de>
2020-01-07 15:43:49 +0100

    Fix sanity studio logo.

--- studio/logo/myLogo.js
+++ studio/logo/myLogo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './myLogo.css';
 
 const myLogo = () => (
-  <img className={styles.myLogo} src="/static/Schafe-vorm-Fenster-UG_Logo_green Avatar-01.png" alt="Schafe vorm Fenster"/>
+  <img className={styles.myLogo} src="/static/schafe-vorm-fenster-logo.png" alt="Schafe vorm Fenster"/>
 );
 
 export default myLogo;
