23d9997eb297a7b3ddccd80daf6fbec83c09c024
Jan-Henrik Hempel <jan@schafe-vorm-fenster.de>
2021-08-04 08:09:12 +0200

    Add population to municipality and community.

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -78,6 +78,12 @@ export default createSchema({
           name: "zip",
           type: "string",
         },
+        {
+          title: "Population",
+          name: "population",
+          type: "number",
+        },
+
       ],
       preview: {
         select: {
@@ -156,6 +162,11 @@ export default createSchema({
           type: "array",
           of: [{ type: "string" }],
           validation: (Rule) => Rule.unique(),
+        },
+        {
+          title: "Population",
+          name: "population",
+          type: "number",
         },
         {
           title: "Municipality",
